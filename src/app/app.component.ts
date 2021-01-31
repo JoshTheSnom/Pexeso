9e5f329dc2dbcae39d2eda9824083ccb9391844a
Kreggy <filip.makrlik@gmail.com>
2021-01-31 22:09:26 +0100

    done?

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+interface Card{
+  icon: string;
+  flipped: boolean;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +12,47 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Pexeso';
+
+  savedCard: Card | null = null;
+
+  cards: Card[] = [
+    {icon: "Apple", flipped: false},
+    {icon: "Watermelon", flipped: false},
+    {icon: "Orange", flipped: false},
+    {icon: "Strawberry", flipped: false},
+    {icon: "Grape", flipped: false},
+    {icon: "Cherry", flipped: false},
+    {icon: "Mango", flipped: false},
+    {icon: "Banana", flipped: false},
+    {icon: "Apple", flipped: false},
+    {icon: "Watermelon", flipped: false},
+    {icon: "Orange", flipped: false},
+    {icon: "Strawberry", flipped: false},
+    {icon: "Grape", flipped: false},
+    {icon: "Cherry", flipped: false},
+    {icon: "Mango", flipped: false},
+    {icon: "Banana", flipped: false}
+
+  ]
+  turn(clickedCard: Card): void {
+      if (clickedCard.flipped === true) {
+        return;
+      }
+      if (this.savedCard === null) {
+        clickedCard.flipped = true;
+        this.savedCard = clickedCard;
+      } else {
+        if (this.savedCard !== clickedCard) {
+          clickedCard.flipped = true;
+          if (clickedCard.icon !== this.savedCard.icon) {
+            const savedCard = this.savedCard;
+            setTimeout(() => {
+              clickedCard.flipped = false;
+              savedCard.flipped = false;
+            }, 750);
+          }
+          this.savedCard = null;
+        }
+      }
+    }
 }
