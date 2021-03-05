1168b14959a99236bed1e7756c02eeec424f30e7
Kreggy <filip.makrlik@gmail.com>
2021-03-05 20:06:03 +0100

    ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {callbackify} from "util";
-import { FormsModule } from "@angular/forms";
+import {FormsModule} from "@angular/forms";
 
-interface Card{
+interface Card {
   icon: string;
   flipped: boolean;
   found: boolean;
@@ -44,75 +44,49 @@ export class AppComponent {
 
   ]
   cards: Card[];
-  smallNumPairs: boolean = false;
-  bigNumPairs: boolean = false;
+
   startGame(pairs: string) {
+    this.won = false;
+    this.hasStarted = true;
 
-
-    this.numPairs = pairs as unknown as number;
-    if (this.numPairs < 2){
-      this.bigNumPairs = false;
-      this.smallNumPairs = true;
-    }
-    if(this.numPairs > 1 && this.numPairs <= this.listCards.length/2) {
-
-      this.smallNumPairs = false;
-      this.bigNumPairs = false;
-
-      this.won = false;
-      this.hasStarted = true;
-
-      this.cards = this.listCards.slice(0, this.numPairs*2);
-      this.cards = this.cards.sort(() =>  Math.random() - 0.5);
-    }
-    else if (this.numPairs < 2){
-      this.bigNumPairs = false;
-      this.smallNumPairs = true;
-    }
-    else {
-      this.smallNumPairs = false;
-      this.bigNumPairs = true;
-    }
+    this.cards = this.listCards.slice(0, this.numPairs * 2);
+    this.cards = this.cards.sort(() => Math.random() - 0.5);
   }
 
 
-
-
-
   turn(clickedCard: Card): void {
-      if (clickedCard.flipped === true) {
-        return;
-      }
-      if (this.savedCard === null) {
+    if (clickedCard.flipped === true) {
+      return;
+    }
+    if (this.savedCard === null) {
+      clickedCard.flipped = true;
+      this.savedCard = clickedCard;
+    } else {
+      if (this.savedCard !== clickedCard) {
         clickedCard.flipped = true;
-        this.savedCard = clickedCard;
-      } else {
-        if (this.savedCard !== clickedCard) {
-          clickedCard.flipped = true;
-          if (clickedCard.icon !== this.savedCard.icon) {
-            const savedCard = this.savedCard;
-            setTimeout(() => {
-              clickedCard.flipped = false;
-              savedCard.flipped = false;
-            }, 500);
-          }
-          else {
-            this.savedCard.found = true;
-            clickedCard.found = true;
-            this.foundPairs++;
-            this.hasWon();
-          }
-          this.savedCard = null;
+        if (clickedCard.icon !== this.savedCard.icon) {
+          const savedCard = this.savedCard;
+          setTimeout(() => {
+            clickedCard.flipped = false;
+            savedCard.flipped = false;
+          }, 500);
+        } else {
+          this.savedCard.found = true;
+          clickedCard.found = true;
+          this.foundPairs++;
+          this.hasWon();
         }
+        this.savedCard = null;
       }
     }
+  }
 
-    hasWon() {
-      if(this.foundPairs == this.numPairs) {
-        setTimeout(()=>{
+  hasWon() {
+    if (this.foundPairs == this.numPairs) {
+      setTimeout(() => {
         this.won = true;
 
-        for(let i = 0; i < this.cards.length; i++) {
+        for (let i = 0; i < this.cards.length; i++) {
           this.cards[i].flipped = false;
           this.cards[i].found = false;
         }
@@ -120,7 +94,7 @@ export class AppComponent {
         this.foundPairs = 0;
         this.savedCard = null;
         this.hasStarted = false;
-        },500);
-      }
+      }, 500);
     }
+  }
 }
